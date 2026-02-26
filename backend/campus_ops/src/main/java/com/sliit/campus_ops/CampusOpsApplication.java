82b653dbbb5b146e8e5a928f31ebde35d1e32fb2
Thushalini Peranantham <it23231450@my.sliit.lk>
2026-02-26 22:42:05 +0530

    backend created and connected to mongodb successfully

---
+++ backend/campus_ops/src/main/java/com/sliit/campus_ops/CampusOpsApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.campus_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusOpsApplication.class, args);
+	}
+
+}
---
+++ backend/campus_ops/src/test/java/com/sliit/campus_ops/CampusOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.campus_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
