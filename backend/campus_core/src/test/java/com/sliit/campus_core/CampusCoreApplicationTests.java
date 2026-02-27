eaa22c339a48923f7eb85e862030dc8756de83b5
Thushalini Peranantham <it23231450@my.sliit.lk>
2026-02-27 23:01:41 +0530

    re install backend

--- backend/campus_core/src/test/java/com/sliit/campus_core/CampusCoreApplicationTests.java
+++ backend/campus_core/src/test/java/com/sliit/campus_core/CampusCoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sliit.campus_ops;
+package com.sliit.campus_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CampusOpsApplicationTests {
+class CampusCoreApplicationTests {
 
 	@Test
 	void contextLoads() {
