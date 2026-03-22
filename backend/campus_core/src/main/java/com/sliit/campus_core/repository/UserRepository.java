66613e29cc57621e850a39a297ae8e82acc086c3
Thushalini Peranantham <it23231450@my.sliit.lk>
2026-03-22 16:02:08 +0530

    adding user andd notification service, Repository and controller

---
+++ backend/campus_core/src/main/java/com/sliit/campus_core/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sliit.campus_core.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.sliit.campus_core.entity.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
