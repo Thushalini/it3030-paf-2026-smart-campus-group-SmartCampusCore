1321e5734017b75d89ea6f53a729e45455e91f62
Thushalini Peranantham <it23231450@my.sliit.lk>
2026-04-20 22:11:31 +0530

    success google login/signup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-// main.jsx
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
@@ -6,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="775821170906-n76oam087h1sdk2lij87dk96h3l08odd.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <AuthProvider>
       <App />
     </AuthProvider>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "unsafe-none"
+    }
+  },
   plugins: [react(), tailwindcss()],
   define: {
     global: 'globalThis', 
